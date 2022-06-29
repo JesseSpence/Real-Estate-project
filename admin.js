b29a6c6b72bd8b65eaf12c549dfdcc8aedf97f81
JesseSpence <jesse2601@icloud.com>
2022-06-29 16:31:40 +0200

    2nd commit

---
+++ admin.js
@@ -0,0 +1,196 @@
+let houses = JSON.parse(localStorage.getItem("houses"))
+? JSON.parse(localStorage.getItem("houses")):[
+    {id:1,
+        image:'https://picsum.photos/200/300/?blur',
+     title:'Light and Modern Apartment',
+     price: '$4500/mo',
+     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     bedrooms:'4',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
+    },
+
+    {id:2,
+        image:'https://picsum.photos/200/300/?blur',
+    title:'Cute Apartment',
+    price: '$4500/mo',
+    address:'2436 SW 8th St, Miami, FL 33135, USA',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:3,
+    image: 'https://picsum.photos/200/300/?blur',
+   title:'Cozy Apartment',
+   price: '$4500/mo',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'4',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:4,
+    image: 'https://picsum.photos/200/300/?blur',
+  title:'Newly Built Apartment',
+  price: '$4500/mo',
+  address:'2436 SW 8th St, Miami, FL 33135, USA',
+  bedrooms:'4',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:5,
+    image: 'https://picsum.photos/200/300/?blur',
+ title:'Fully Furnished Apartment',
+ price: '$4500/mo',
+ address:'2436 SW 8th St, Miami, FL 33135, USA',
+ bedrooms:'4',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},
+{id:6,
+    image: 'https://picsum.photos/200/300/?blur',
+     title:'North Facing House',
+     price: '$4500/mo',
+     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     bedrooms:'4',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
+    },
+    {id:7,
+        image: 'https://picsum.photos/200/300/?blur',
+    title:'Newly Built House',
+    price: '$4500/mo',
+    address:'2436 SW 8th St, Miami, FL 33135, USA',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:8,
+    image: 'https://picsum.photos/200/300/?blur',
+   title:'Grand Mansion',
+   price: '$4500/mo',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'4',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:9,
+    image: 'https://picsum.photos/200/300/?blur',
+  title:'Bachelor Pad',
+  price: '$4500/mo',
+  address:'2436 SW 8th St, Miami, FL 33135, USA',
+  bedrooms:'4',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:10,
+    image: 'https://picsum.photos/200/300/?blur',
+ title:'Penthouse Open Floor',
+ price: '$4500/mo',
+ address:'2436 SW 8th St, Miami, FL 33135, USA',
+ bedrooms:'4',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},]
+
+localStorage.setItem("houses", JSON.stringify(houses));
+
+    const propertiesTable = document.querySelector("#propertylist");
+
+function ShowHouseListings (houses) {
+    document.querySelector("#propertylist").innerHTML = "";
+  
+    houses.forEach(house => {
+      propertiesTable.innerHTML += `
+        <tr>
+        <td>${house.id}</td>
+          <td><img src=${house.image}</td>
+          <td>${house.title}</td>
+          <td>${house.address}</td>
+          <td>${house.type}</td>
+          <td>${house.bedrooms}</td>
+          <td>${house.bathrooms}</td>
+          <td>${house.garage}</td>
+          <td>${house.size}sq ft</td>
+          <td>${house.price}</td>
+          <td><button onclick="editHouse(${house.id})">✎</button> </td><td><button onclick="deleteHouse(${house.id})">✂</button></td>
+        <tr>
+      `;
+    })
+  };
+  
+ShowHouseListings(houses);
+
+// delete function
+function deleteHouse(id) {
+  houses = houses.filter((house) => {
+    return house.id !== id;     //loop through houses and return houses which id noes not match
+  });   
+  localStorage.setItem("houses", JSON.stringify(houses));
+  ShowHouseListings(houses);
+}
+
+
+// edit house listing 
+function editHouse(id) {
+  let house = houses.find((house) => house.id === id); //Finds the id of button that was clicked
+  let input = prompt("Input Changes"); //Allows the user to input their own changes
+  house.title = input; //Changes the content to what the user's input
+  // addToStorage(objectives); //Adds to local storage
+  ShowHouseListings(houses);
+}
+
+
+
+
+// let index = houses.indexOf(house => {houses.id ===id})
+// houses= houses.slice(index,1);
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let houses = JSON.parse(localStorage.getItem("houses"))
 ? JSON.parse(localStorage.getItem("houses")):[
-    {image: 'https://picsum.photos/200/300/?blur',
+    {id:1,
+        image: 'https://picsum.photos/200/300/?blur',
      title:'Light and Modern Apartment',
      price: '$4500/mo',
      address:'2436 SW 8th St, Miami, FL 33135, USA',
      bedrooms:'4',
      bathrooms:'2',
-     Garage:'1',
+     garage:'1',
      size:'1200',
      type:'Apartment',
      user:' Samuel Palmer',
@@ -14,8 +15,9 @@ let houses = JSON.parse(localStorage.getItem("houses"))
      yearBuilt:'2016',
     },
 
-    {image:'https://picsum.photos/200/300/?blur',
-    title:'Light and Modern Apartment',
+    {id:2,
+        image:'https://picsum.photos/200/300/?blur',
+    title:'Cute Apartment',
     price: '$4500/mo',
     address:'2436 SW 8th St, Miami, FL 33135, USA',
     bedrooms:'4',
@@ -26,9 +28,121 @@ let houses = JSON.parse(localStorage.getItem("houses"))
     user:' Samuel Palmer',
     datePosted:'04/04/2020',
     yearBuilt:'2016',
-   },]
+   },
+   {id:3,
+    image: 'https://picsum.photos/200/300/?blur',
+   title:'Cozy Apartment',
+   price: '$4500/mo',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'4',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:4,
+    image: 'https://picsum.photos/200/300/?blur',
+  title:'Newly Built Apartment',
+  price: '$4500/mo',
+  address:'2436 SW 8th St, Miami, FL 33135, USA',
+  bedrooms:'4',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:5,
+    image: 'https://picsum.photos/200/300/?blur',
+ title:'Fully Furnished Apartment',
+ price: '$4500/mo',
+ address:'2436 SW 8th St, Miami, FL 33135, USA',
+ bedrooms:'4',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},
+{id:6,
+    image: 'https://picsum.photos/200/300/?blur',
+     title:'North Facing House',
+     price: '$4500/mo',
+     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     bedrooms:'4',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
+    },
+    {id:7,
+        image: 'https://picsum.photos/200/300/?blur',
+    title:'Newly Built House',
+    price: '$4500/mo',
+    address:'2436 SW 8th St, Miami, FL 33135, USA',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:8,
+    image: 'https://picsum.photos/200/300/?blur',
+   title:'Grand Mansion',
+   price: '$4500/mo',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'4',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:9,
+    image: 'https://picsum.photos/200/300/?blur',
+  title:'Bachelor Pad',
+  price: '$4500/mo',
+  address:'2436 SW 8th St, Miami, FL 33135, USA',
+  bedrooms:'4',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:10,
+    image: 'https://picsum.photos/200/300/?blur',
+ title:'Penthouse Open Floor',
+ price: '$4500/mo',
+ address:'2436 SW 8th St, Miami, FL 33135, USA',
+ bedrooms:'4',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},];
 
-   const propertiesContainer = document.querySelector("#properties")
+   const propertiesContainer = document.querySelector("#properties");
 
 // add array to local storage   
 //    localStorage.setItem("houses", JSON.stringify(houses));
@@ -40,20 +154,23 @@ function showHouses (houses) {
     houses.forEach(house => {
         propertiesContainer.innerHTML += `
             <div id='housecards'>
+            <span id="heart" class="heart"><i onclick="wishList()" class="fas fa-heart"></i></span>
                 <img src=${ house.image } />
                  <h3>${house.title}</h3>
-                 <ul id=house.values>
-                 <li>${house.address}</li>
+                 <p>${house.address}</p>
+                 <ul id=house-values>
+        
                  <li><i class="fa-solid fa-bed"></i>${house.bedrooms}</li>
                  <li><i class="fa-solid fa-shower"></i>${house.bathrooms}</li>
-                 
-                 <li><i class="fa-solid fa-car"></i>${house.garage}</li>
+                <li><i class="fa-solid fa-car"></i>${house.garage}</li>
+                <li><i class="fa-solid fa-ruler"></i>${house.size}</li>
+                
 
 
             </div>
         `
     })
-}
+};
 
 showHouses(houses);
 
@@ -77,5 +194,4 @@ showHouses(houses);
 // for (let i = 0; houses[i]; i++);
 // document.getElementById("properties").innerHTML = houses;}
 
-// createProperties();       
-
+// createProperties();
